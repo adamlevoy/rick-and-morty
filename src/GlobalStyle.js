5b9a03331bf1eac8051df96741b3b59d56edc7a5
Adam Levoy <ajlevoy86@gmail.com>
2021-11-01 08:31:57 -0700

    added Amatic SC font

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -21,9 +21,7 @@ export const GlobalStyle = createGlobalStyle`
 
 body {
   margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
+  font-family: 'Amatic SC', cursive, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   background-color: var(--green);
